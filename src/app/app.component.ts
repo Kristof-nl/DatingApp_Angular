3123d1d0d722bdda983e1b558288de75008a8b56
Krzysztof Hermaszewski <hermaszewski.krzysztof@gmail.com>
2022-04-06 21:08:52 +0200

    Added register to account service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,9 @@ export class AppComponent implements OnInit {
   title = 'The Dating App';
   users: any;
 
-  constructor(private http: HttpClient, private accountService: AccountService) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit() {
-   this.getUsers();
    this.setCurrentUser();
   }
 
@@ -26,13 +25,6 @@ export class AppComponent implements OnInit {
     
   }
 
-  getUsers()  {
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-    this.users = response;
-    }, error => {
-      console.log(error);
-    })
-  }
 }
 
 
