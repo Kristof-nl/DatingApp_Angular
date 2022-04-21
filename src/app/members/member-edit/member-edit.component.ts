c7cab94ae3845fb295638065620c5a540867451f
Krzysztof Hermaszewski <hermaszewski.krzysztof@gmail.com>
2022-04-21 22:28:23 +0200

    Changes in edit component

--- src/app/members/member-edit/member-edit.component.ts
+++ src/app/members/member-edit/member-edit.component.ts
@@ -13,10 +13,16 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm: NgForm;
   member: Member;
   user: User;
+  @HostListener('window:beforeunload', ['$event']) unloadNotification($event: any) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
 
-  constructor(private accountService: AccountService, private memberService: MembersService) { 
+  constructor(private accountService: AccountService, private memberService: MembersService, private toastr: ToastrService) { 
       this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
   }
 
@@ -29,5 +35,11 @@ export class MemberEditComponent implements OnInit {
   this.memberService.getMember(this.user.userName).subscribe(member => {
   this.member = member;
   })
+  }
+
+  updateMember() {
+    console.log(this.member);
+    this.toastr.success('Profile updated succesfully');
+    this.editForm.reset(this.member)
   }
 }
